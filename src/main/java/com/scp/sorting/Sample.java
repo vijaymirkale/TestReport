609066f219aa1bdca7f4514596e7ae6ae5480a92
yogymax <yogymax@gmail.com>
2019-04-08 16:02:25 +0530

    first commit

--- src/main/java/com/scp/sorting/Sample.java
+++ src/main/java/com/scp/sorting/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("This is newly created file....105!");
 		System.out.println("This is newly created file....107!");
+		System.out.println("This is newly created file....107!");
 		System.out.println("This is newly created file....!");
 		System.out.println("This is newly created file....1!");
 		System.out.println("This is newly created file....2!");
