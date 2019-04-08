fe6714520106e0fe0b998974dc6c48efc4e4f1a6
yogymax <yogymax@gmail.com>
2019-04-08 15:32:02 +0530

    both commits..push conflicts resolution

--- src/main/java/com/scp/sorting/Sample.java
+++ src/main/java/com/scp/sorting/Sample.java
@@ -5,5 +5,6 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("This is newly created file....!");
 		System.out.println("This is newly created file....1!");
+		System.out.println("This is newly created file....2!");
 	}
 }
