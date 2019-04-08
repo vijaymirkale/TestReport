58e8f20a2cbec29f800f04d10a5293f8f47a9a93
yogymax <yogymax@gmail.com>
2019-04-08 15:52:20 +0530

    final commit and push

--- src/main/java/com/scp/sorting/Sample.java
+++ src/main/java/com/scp/sorting/Sample.java
@@ -3,12 +3,14 @@ package com.scp.sorting;
 public class Sample {
 
 	public static void main(String[] args) {
+		System.out.println("This is newly created file....105!");
 		System.out.println("This is newly created file....!");
 		System.out.println("This is newly created file....1!");
 		System.out.println("This is newly created file....2!");
+		System.out.println("This is newly created file....105!");
 		System.out.println("This is newly created file....3!");
 		System.out.println("This is newly created file....4!");
 		System.out.println("This is newly created file....101!");
-		
+		System.out.println("This is newly created file....105!");
 	}
 }
