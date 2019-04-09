0ceba054bb4885e3053b5840caf713b3b5bac041
vijaymirkale <mirkalevijay@gmail.com>
2019-04-09 11:14:01 +0530

    second commit

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -2,6 +2,10 @@ package com.scp.sorting;
 
 public class TestSorting {
 		public static void main(String[] args) {
+			int i=20;
+			int j=20;
+			int result=i+j;
+			System.out.println("result"+result);
 			
 		
 			System.out.println("welcimeto GIT");
